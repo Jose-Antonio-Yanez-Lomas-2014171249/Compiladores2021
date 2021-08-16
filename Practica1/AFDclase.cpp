ce42e2ea9e8243885c2520ad259780984d92f135
Jose-Antonio-Yanez-Lomas-2014171249 <85587095+Jose-Antonio-Yanez-Lomas-2014171249@users.noreply.github.com>
2021-08-16 17:52:45 -0500

    Add files via upload

---
+++ Practica1/AFDclase.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+class AFN{
+    private int Einicial;
+    private int Efinal;
+    private char alfabeto[10];
+    
+    public void FuncionTrancision{
+        
+    }
+
+}
